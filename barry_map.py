4db6bebb3743928f031808b98e985230fc895b2e
neverGonnaGiveUup123 <102629343+neverGonnaGiveUup123@users.noreply.github.com>
2022-05-27 12:10:31 +1200

    Updated: Added more markers
    Added markers showing the locations of the photos I took.

--- barry_map.py
+++ barry_map.py
@@ -1,10 +1,17 @@
 import folium
 
-
-map = folium.Map(location=[-36.967355, 174.918153], tiles="CartoDB Positron", zoom_start=20)
+map = folium.Map(location=[-36.967355, 174.918153], tiles="OpenStreetMap", zoom_start=20)
 
 fg = folium.FeatureGroup(name="BCP_map")
+
 fg.add_child(folium.Marker(location=[-36.967355, 174.918153], popup="Ormiston Senior College", icon=folium.Icon(color='red')))
+
+photo_locations = [[-36.964575, 174.908148],[-36.965038, 174.908091],[-36.965219, 174.907938],[-36.966422, 174.911517]]
+
+descriptions = ['Bacteria', 'Bacteria', 'Trolley', 'General litter']
+
+for i, j in zip(photo_locations, descriptions):
+    fg.add_child(folium.CircleMarker(radius=10, opacity=0.7, location= i, popup= j, color='red', fill_color='red'))
 
 
 map.add_child(fg)
